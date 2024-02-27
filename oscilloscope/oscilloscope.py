5288ce52bc82d64c7ccb82bd3fa71e93cb156886
Alex Rizzoni <rizzoni.alex@gmail.com>
2024-02-26 19:11:18 -0500

    big refactoring - finalize abstract class structure

--- oscilloscope/oscilloscope.py
+++ oscilloscope/oscilloscope.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3.11
 """
-_summary
 bench/oscilloscope/oscilloscope.py - Oscilloscope class for the bench module
 
 TODO
-trigger
-vertical range
-horizontal timebase
-attenuation
-coupling
-offset
-waveform inversion
-impedance
-signal type
-bwlimit
-get waveform
+Trigger
+    
+Display
+    Label
+    Clear
+Channel
+    Attenuation
+    Coupling
+    Impedance
+    Signal Type
+    BW Limit
+Waveform
+    Vertical Range
+    Horizontal Timebase
+    Offset
+    Waveform Inversion
+Measure
 """
 
+from abc import ABC, abstractmethod
 from typing import Self
 
 from .. import bench
 
-class Oscilloscope(bench.Instrument):
+class Oscilloscope(bench.Instrument, ABC):
     """
     Class containing interface for oscilloscopes.
     Inherits from bench.Instrument. Abstract base class, should not be 
@@ -39,30 +45,23 @@ class Oscilloscope(bench.Instrument):
 
         super().__init__(address)  # call parent class constructor for basic init
 
-        self.channels: int = self.__get_channels() # Returns array of channel numbers
+        self.channels: list(int) = self.get_channels() # Returns array of channel numbers
+        self.current_channel = 1
 
-    def lock(self) -> Self: # move to instrument
+    def __subclasshook__(self) -> True or False or NotImplemented:
         """
-        Disable front panel controls
-        """
-
-        self.__lock()  # disable front panel controls
-        return self
-
-    def unlock(self) -> Self: # move to instrument
-        """
-        Enable front panel controls
+        TODO: Test if a class is a subclass of Oscilloscope.
         """
 
-        self.__unlock()  # disable front panel controls
-        return self
+        return NotImplemented
+
 
     def channel(self, channel_number: int) -> Self | None: # move to instrument
         """
         sets the oscilloscope channel to operate on
 
         Arguments:
-        sewqa5r    channel_number -- number of channel to select
+            channel_number -- number of channel to select
 
         Returns:
             self - returns parent object for second method call
@@ -71,29 +70,10 @@ class Oscilloscope(bench.Instrument):
         if channel_number in self.channels:
             self.current_channel = channel_number # set current_channel
         else:
-            raise Exception("Channel Number Error: Channel out of range")
+            raise ValueError("Channel Number Error: Channel out of range")
+
         return self  # return instance
 
-    def set_label(self, label: str) -> Self:
-        """
-        Writes waveform labels to the oscilloscope screen
-
-        Arguments:
-            label -- label text, written to oscilloscope screen
-        """
-
-        self.__set_label(self.current_channel, label)
-        return self
-
-    def autoscale(self) -> Self:
-        """
-        Autoscales current channel
-        """
-
-        self.__autoscale()
-        return self
-
-    # Get Input Waveform Parameters
 
     def get_input_parameters(self) -> Self:
         """
@@ -126,26 +106,25 @@ class Oscilloscope(bench.Instrument):
         """
 
         return [
-                self.__get_input_n_bits(), 
-                self.__get_input_binary_format(), 
-                self.__get_input_n_bytes(), 
-                self.__get_input_byte_order(), 
-                self.__get_input_composition(),
-                self.__get_input_encoding(), 
-                self.__get_input_filter_frequency(), 
-                self.__get_input_n_points(),
-                self.__get_input_point_format(),
-                self.__get_input_point_offset(),
-                self.__get_input_x_increment(),
-                self.__get_input_x_unit(),
-                self.__get_input_x_zero(),
-                self.__get_input_y_multiplier(),
-                self.__get_input_y_offset(),
-                self.__get_input_y_unit(),
-                self.__get_input_y_zero()
+                self.get_input_n_bits(),
+                self.get_input_binary_format(),
+                self.get_input_n_bytes(),
+                self.get_input_byte_order(),
+                self.get_input_composition(),
+                self.get_input_encoding(),
+                self.get_input_filter_frequency(),
+                self.get_input_n_points(),
+                self.get_input_point_format(),
+                self.get_input_point_offset(),
+                self.get_input_x_increment(),
+                self.get_input_x_unit(),
+                self.get_input_x_zero(),
+                self.get_input_y_multiplier(),
+                self.get_input_y_offset(),
+                self.get_input_y_unit(),
+                self.get_input_y_zero()
                 ]
 
-    # Get Output Waveform Parameters
 
     def get_output_parameters(self) -> Self: # move to oscilloscope
         """
@@ -178,126 +157,540 @@ class Oscilloscope(bench.Instrument):
         """
 
         return [
-                self.__get_output_n_bits(), 
-                self.__get_output_binary_format(), 
-                self.__get_output_n_bytes(), 
-                self.__get_output_byte_order(), 
-                self.__get_output_composition(),
-                self.__get_output_encoding(), 
-                self.__get_output_filter_frequency(), 
-                self.__get_output_n_points(),
-                self.__get_output_point_format(),
-                self.__get_output_point_offset(),
-                self.__get_output_x_increment(),
-                self.__get_output_x_unit(),
-                self.__get_output_x_zero(),
-                self.__get_output_y_multiplier(),
-                self.__get_output_y_offset(),
-                self.__get_output_y_unit(),
-                self.__get_output_y_zero()
+                self.get_output_n_bits(),
+                self.get_output_binary_format(),
+                self.get_output_n_bytes(),
+                self.get_output_byte_order(),
+                self.get_output_composition(),
+                self.get_output_encoding(),
+                self.get_output_filter_frequency(),
+                self.get_output_n_points(),
+                self.get_output_point_format(),
+                self.get_output_point_offset(),
+                self.get_output_x_increment(),
+                self.get_output_x_unit(),
+                self.get_output_x_zero(),
+                self.get_output_y_multiplier(),
+                self.get_output_y_offset(),
+                self.get_output_y_unit(),
+                self.get_output_y_zero()
                 ]
 
-    # Set Input Waveform Parameters
 
-    def set_input_parameters(self, n_bits, binary_format, n_bytes, byte_order, composition,
-                         encoding, frequency, n_points, point_format, point_offset,
-                         x_increment, x_unit, x_zero, y_multiplier, y_offset, y_unit,
-                         y_zero) -> Self:
-        if n_bits:
-            self.__set_input_n_bits(n_bits)
-        if binary_format:
-            self.__set_input_binary_format(format)
-        if n_bytes:
-            self.__set_input_n_bytes(n_bytes)
-        if byte_order:
-            self.__set_input_byte_order(byte_order)
-        if composition:
-            self.__set_input_composition(composition)
-        if encoding:
-            self.__set_input_encoding(encoding)
-        if frequency:
-            self.__set_input_filter_frequency(frequency)
-        if n_points:
-            self.__set_input_n_points(n_points)
-        if point_format:
-            self.__set_input_point_format(point_format)
-        if point_offset:
-            self.__set_input_point_offset(point_offset)
-        if x_increment:
-            self.__set_input_x_increment(x_increment)
-        if x_unit:
-            self.__set_input_x_unit(x_unit)
-        if x_zero:
-            self.__set_input_x_zero(x_zero)
-        if y_multiplier:
-            self.__set_input_y_multiplier(y_multiplier)
-        if y_offset:
-            self.__set_input_y_offset(y_offset)
-        if y_unit:
-            self.__set_input_y_unit(y_unit)
-        if y_zero:
-            self.__set_input_y_zero(y_zero)
-
-# still need to write methods but have them raise this error instead
-#   def methodB(self):
-#       raise NotImplementedError("Must override methodB")
-# stackoverflow.com/questions/25062114/calling-child-class-method-from-parent-class-file-in-python
+    @abstractmethod
+    def lock(self) -> Self: # move to instrument
+        """
+        Disable the instrument's front panel controls.
+        """
 
 
+    @abstractmethod
+    def unlock(self) -> Self: # move to instrument
+        """
+        Enable the instrument's front panel controls.
+        """
 
 
-# TODO
+    @abstractmethod
+    def set_label(self, label: str) -> Self:
+        """
+        Writes waveform labels to the oscilloscope screen for the current
+        channel.
 
-    def set_trigger_parameters(): #TODO
-        return 0
+        Arguments:
+            label -- label text, written to oscilloscope screen
+        """
 
-    def get_trigger_parameters(): #TODO
-        return 0
-    
-    def trigger(): # TODO
-        return 0
 
-    def set_input_attenuation(): #TODO
-        return 0
+    @abstractmethod
+    def autoscale(self) -> Self:
+        """
+        Send built-in autoscale command to the instrument. Note that different
+        scopes may implement different behavior. It may be beneficial to define
+        the desired behavior explicitly using other commands.
+        """
 
-    def get_input_attenuation(): #TODO
-        return 0
 
-    def set_input_coupling(): #TODO
-        return 0
+    @abstractmethod
+    def get_channels(self) -> Self:
+        """
+        Returns the number of channels the current instrument supports.
+        """
 
-    def get_input_coupling(): #TODO
-        return 0
 
-    def set_input_impedance(): #TODO
-        return 0
+    @abstractmethod
+    def get_input_n_bits(self) -> int:
+        """
+        Gets the number of bits per binary waveform point for the
+        incoming waveform.
+        """
 
-    def get_input_impedance(): #TODO
-        return 0
 
-    def set_signal_type(): #TODO
-        return 0
+    @abstractmethod
+    def get_input_binary_format(self) -> int:
+        """
+        Gets the format of binary data for the incoming waveform. Signed
+        integer (RI) or positive/unsigned integer (RP).
+        """
 
-    def get_signal_type(): #TODO
-        return 0
 
-    def set_acquisition_mode(): #TODO
-        return 0
+    @abstractmethod
+    def get_input_n_bytes(self) -> int:
+        """
+        
+        """
 
-    def get_acquisition_mode(): #TODO
-        return 0
 
-    def set_horizontal_scale(): #TODO
-        return 0
+    @abstractmethod
+    def get_input_byte_order(self) -> int:
+        """
+        
+        """
 
-    def get_horizontal_scale(): #TODO
-        return 0
 
-    def set_vertical_scale(): #TODO
-        return 0
+    @abstractmethod
+    def get_input_composition(self) -> int:
+        """
+        Returns the type of data the QUERY command will give.
+        """
 
-    def get_vertical_scale(): #TODO
-        return 0
 
-    def get_waveform(): #TODO
-        return 0
+    @abstractmethod
+    def get_input_encoding(self) -> int:
+        """
+        
+        """
+
+
+    @abstractmethod
+    def get_input_filter_frequency(self) -> int:
+        """
+        Returns the cutoff frequency of the oscilloscope channel's input filter.
+        """
+
+
+    @abstractmethod
+    def get_input_n_points(self) -> int:
+        """
+        Returns the number of data points in the waveform data input buffer.
+        """
+
+
+    @abstractmethod
+    def get_input_point_format(self) -> int:
+        """
+        Returns the format of the data points in the waveform data input buffer.
+        """
+
+
+    @abstractmethod
+    def get_input_point_offset(self) -> int:
+        """
+        Returns x-offset of data points in the waveform data input buffer.
+        """
+
+
+    @abstractmethod
+    def get_input_x_increment(self) -> int:
+        """
+        Returns x-scale of the waveform data.
+        """
+
+
+    @abstractmethod
+    def get_input_x_unit(self) -> int:
+        """
+        Returns x-unit of the waveform data.
+        """
+
+
+    @abstractmethod
+    def get_input_x_zero(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_y_multiplier(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_y_offset(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_y_unit(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_y_zero(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_n_bits(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_binary_format(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_n_bytes(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_byte_order(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_composition(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_encoding(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_filter_frequency(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_n_points(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_point_format(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_point_offset(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_x_increment(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_x_unit(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_x_zero(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_y_multiplier(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_y_offset(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_y_unit(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_output_y_zero(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_binary_format(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_n_bytes(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_byte_order(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_composition(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_encoding(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_filter_frequency(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_n_points(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_point_format(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_point_offset(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_x_increment(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_x_unit(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_x_zero(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_y_multiplier(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_y_offset(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_y_unit(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_y_zero(self) -> int:
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_trigger_parameters(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_trigger_parameters(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def trigger(self): # TODO
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_attenuation(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_attenuation(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_coupling(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_coupling(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_input_impedance(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_input_impedance(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_signal_type(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_signal_type(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_acquisition_mode(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_acquisition_mode(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_horizontal_scale(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_horizontal_scale(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def set_vertical_scale(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_vertical_scale(self):
+        """
+
+        """
+
+
+    @abstractmethod
+    def get_waveform(self):
+        """
+
+        """
